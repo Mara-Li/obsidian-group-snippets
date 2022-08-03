8c312d5eef45d96c187a815c60e8415bf8527a68
Mara <mara-li@outlook.fr>
2022-08-03 16:52:43 +0200

    chore: add beta

---
+++ version-beta.mjs
@@ -0,0 +1,10 @@
+import { readFileSync, writeFileSync } from "fs";
+
+let packageFile = JSON.parse(readFileSync("package.json", "utf8"));
+console.log('Package version : ' + packageFile.version);
+let manifest = JSON.parse(readFileSync("manifest-beta.json", "utf8"));
+console.log('Manifest version : ' + manifest.version);
+const targetVersion = packageFile.version;
+manifest.version = targetVersion;
+console.log('Updating manifest-beta with version : ' + manifest.version);
+writeFileSync("manifest-beta.json", JSON.stringify(manifest, null, "\t"));
