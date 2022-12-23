f29c020b2217266787fa61ce6166704ce401a742
Mara <mara-li@outlook.fr>
2022-12-23 15:40:14 +0100

    refactor: move style.css to plugin folder

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,6 +1,7 @@
 import esbuild from "esbuild";
 import process from "process";
 import builtins from 'builtin-modules'
+import fs from 'fs'
 
 const banner =
 `/*
@@ -11,45 +12,30 @@ if you want to view the source, please visit the github repository of this plugi
 
 const prod = (process.argv[2] === 'production');
 
+const moveStyles = {
+	name: 'move-styles',
+	setup(build) {
+		build.onEnd(() => {
+			fs.copyFileSync('./plugin/styles.css', './styles.css');
+		});
+	}
+}
+
+
 esbuild.build({
 	banner: {
 		js: banner,
 	},
-	entryPoints: ['plugin/main.ts'],
+	entryPoints: ['plugin/main.ts', 'plugin/styles.css'],
 	bundle: true,
-	external: [
-		'obsidian',
-		'electron',
-		'@codemirror/autocomplete',
-		'@codemirror/closebrackets',
-		'@codemirror/collab',
-		'@codemirror/commands',
-		'@codemirror/comment',
-		'@codemirror/fold',
-		'@codemirror/gutter',
-		'@codemirror/highlight',
-		'@codemirror/history',
-		'@codemirror/language',
-		'@codemirror/lint',
-		'@codemirror/matchbrackets',
-		'@codemirror/panel',
-		'@codemirror/rangeset',
-		'@codemirror/rectangular-selection',
-		'@codemirror/search',
-		'@codemirror/state',
-		'@codemirror/stream-parser',
-		'@codemirror/text',
-		'@codemirror/tooltip',
-		'@codemirror/view',
-		'@lezer/common',
-		'@lezer/highlight',
-		'@lezer/lr',
-		...builtins],
+	external: ['obsidian', 'electron', ...builtins],
+	sourcemap: prod ? false : "inline",
 	format: 'cjs',
 	watch: !prod,
 	target: 'es2016',
 	logLevel: "info",
-	sourcemap: prod ? false : 'inline',
+	minify: prod,
 	treeShaking: true,
-	outfile: 'main.js',
+	outdir: './',
+	plugins: [moveStyles],
 }).catch(() => process.exit(1));
